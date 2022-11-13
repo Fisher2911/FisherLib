9242c8cff9d48bf120eff21371ff9c9dcc4c2f9b
Fisher2911 <fisher1205l@gmail.com>
2022-11-12 22:00:13 -0500

    Added tool chain version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
 
 group = "io.github.fisher2911"
 version = "1.0.0-beta"
-java.sourceCompatibility = JavaVersion.VERSION_17
-java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -43,6 +41,12 @@ publishing {
             from(components["java"])
             artifact(tasks["shadowJar"])
         }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 

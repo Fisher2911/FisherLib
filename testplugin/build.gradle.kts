7bb45706441d98024b602842a2966bc54c5d4994
Fisher2911 <fisher1205l@gmail.com>
2023-07-21 23:27:17 -0400

    Set toolchain java version of testplugin

--- testplugin/build.gradle.kts
+++ testplugin/build.gradle.kts
@@ -40,8 +40,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 tasks {
